15c6109c0c4c8d42bb64e0f3150666b015f87037
zhoujunlin <zhoujunlin@mzhtechnologies.com>
2020-08-20 17:52:42 +0800

    update: add repository in domain

---
+++ post/src/main/java/org/zjl/jeddit/forum/domain/model/aggregates/PostRepository.java
@@ -0,0 +1,31 @@
+package org.zjl.jeddit.forum.domain.model.aggregates;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.zjl.jeddit.forum.infrustructure.mapper.PostMapper;
+import org.zjl.jeddit.forum.infrustructure.repository.mongo.PostMongoRepository;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Junlin Zhou
+ */
+@AllArgsConstructor
+@Component
+public class PostRepository {
+
+    private final PostMongoRepository postRepo;
+    private final PostMapper postMapper;
+
+
+    public Mono<Post> save(Post entity) {
+        org.zjl.jeddit.forum.infrustructure.repository.mongo.Post mongoEntity = postMapper.modelToMongoEntity(entity);
+        return postRepo.save(mongoEntity)
+                .map(postMapper::mongoEntityToModel);
+    }
+
+    public Mono<Post> findById(String id) {
+        return postRepo.findById(id)
+                .map(postMapper::mongoEntityToModel);
+    }
+
+}
